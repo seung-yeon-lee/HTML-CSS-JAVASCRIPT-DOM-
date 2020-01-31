2ec9b52cff0a0213c45b36ea561578bfac0d4de4
Lee <lsy63681@naver.com>
2020-01-31 15:32:30 +0900

    Simple WebSite

---
+++ HTML_CSS/index.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', ()=>{
+    const sign = document.querySelector('.sign-btn');
+
+    sign.addEventListener('click', ()=>{
+        alert('회원가입/로그인 화면 구현 예정')
+    })
+})
